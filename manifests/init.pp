79ddf618aafebbaaed814c6d2829cd6fcbac403c
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-25 21:24:06 +0100

    create zones

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,14 @@ class profile_bind (
   Enum['yes', 'no']  $dnssec          = 'yes',
   Boolean            $manage_firewall = true,
   Hash[String, Hash] $zones           = {},
+  Hash[String, Hash] $zones_defaults  = {},
   Hash[String, Hash] $keys            = {},
 ) {
   class { 'dns':
     forwarders    => $forwarders,
     dnssec_enable => $dnssec,
   }
+  create_resources(dns::zones, $zones, $zones_defaults)
   if $manage_firewall {
     firewall { '00053 allow bind':
       dport  => 53,
