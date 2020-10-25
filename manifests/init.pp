005703c2f83325c9e8885a5a0919a4cc0b51b286
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-25 19:45:15 +0100

    change dns module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,26 +2,16 @@
 #
 #
 class profile_bind (
-  Array[Stdlib::IP::Address] $forwarders      = ['8.8.8.8', '8.8.4.4'],
-  Boolean                    $dnssec          = true,
-  Hash                       $bind_keys       = {},
-  Hash                       $bind_acls       = {},
-  Hash                       $bind_zones      = {},
-  Hash                       $bind_views      = {},
-  Hash                       $bind_records    = {},
-  Boolean                    $manage_firewall = true,
+  Array[String]      $forwarders      = ['8.8.8.8', '8.8.4.4'],
+  Enum['yes', 'no']  $dnssec          = true,
+  Boolean            $manage_firewall = true,
+  Hash[String, Hash] $zones           = {},
+  Hash[String, Hash] $keys            = {},
 ) {
-  class { 'bind':
-    forwarders => $forwarders,
-    dnssec     => $dnssec,
-    version    => 'Controlled by Puppet',
+  class { 'dns':
+    forwarders    => $forwarders,
+    dnssec_enable => $dnssec,
   }
-  create_resources(bind::key, $bind_keys)
-  create_resources(bind::acl, $bind_acls)
-  create_resources(bind::zone, $bind_zones)
-  create_resources(bind::view, $bind_views)
-  create_resources(resource_record, $bind_records)
-
   if $manage_firewall {
     firewall { '00053 allow bind':
       dport  => 53,
